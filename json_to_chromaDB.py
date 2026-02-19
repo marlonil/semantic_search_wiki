bf6f39a33e7a042e7e900da644e0d2d472d347fb
marlon <marlonil@users.noreply.github.com>
2026-02-19 22:30:45 +0100

    feat: Recursive search implementation
    I implemented a function with which I can find a key I am searching for in my Json.
    I need to handel neasted json like the json test.json. There are dicts in dicts and also lists and basic data types. In my case I alwasy need to find the 'final' values like a string and so on. For that a recursive search is a MUST.

--- json_to_chromaDB.py
+++ json_to_chromaDB.py
@@ -1,26 +1,65 @@
 from sentence_transformers import SentenceTransformer
 import json
+import ijson
 
+"""Needs!
+
+-> Helperfunction: The function will go through every json, every time it findes a field names value, it saves the text. 
+
+-> memory crash fixing (json.load will crash my memory with 70gb jsons
+    -> Streaming (ijson): Opens file, Takes everything out of one artikel and extrects the text, 
+    chunks it embed it and saves it in db. So all in one loop
+
+Reminder: ijson walkes through the hole json file and just takes one object. 
+
+-> I need to load 3 sentences at once in the place of one index
+"""
+
+def recursive_search(json_data: dict, target_key: str):
+    """Recursively search a dict for target_key
+
+    Inputs: dict out of a json with dicts, lists and basic data types.
+    Outputs: ["value1", "value2", "value3"] -> The values of the keys we target
+    """
+    recursive_results = [] # -> initializes the results (values of the targeted key)
+    if isinstance(json_data, dict): # -> checks if the json_data is a dict, if so we will become a true and statement is finished
+        for key, value in json_data.items():
+            if key == target_key: # -> checks if the key is the targeted key, we search for
+                recursive_results.append(value) # if so, we will add it to the results
+            recursive_results.extend(recursive_search(value, target_key)) # -> if not we just extend the results with a new iteration will calling the function we currently in. Why extend? If we would add, we would create a empty list which would overrwite everything
+    elif isinstance(json_data, list): # -> we check if the current json data is a list
+        for value in json_data:
+            recursive_results.extend(recursive_search(value, target_key)) # -> we add every item to the list and call the function again to add the value finally to the results.
+# the recall of the function inside the function will cause that we create like a recursive list which will search deeper in the json and also add everything found in one "tree"
+    elif isinstance(json_data, (int, float, bool, str)):
+        print(f"3. Check (basis-typ) json_data: {json_data}")
+
+    return recursive_results
+
+def process_an_object(v1, v2):
+    print(f"v1 = {v1}, \n v2 = {v2}")
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
 with open('test.json') as f:
     data = json.load(f)
 print(json.dumps(data, indent=4))
 y = json.loads(json.dumps(data, indent=4))
-text = y["sections"][2]["has_parts"][1]["value"]
 
-lenght = len(text)
-print(f"lenght: {lenght}")
+raw_recursive = recursive_search(y, "value")
 
-word = (i.strip() for i in text.split("."))
+for sentence in raw_recursive:
+    if isinstance(sentence, (int, float)):
+        raw_recursive.remove(sentence)
 
-sentence = []
-for w in word:
-    sentence.append(w)
+results = []
+for sentence in range(0, len(raw_recursive), 3):
+    # searches every sentence from 0 to the length of all sentences in raw_recursive, but only 3 at once.
+    block = raw_recursive[sentence:sentence+3]
+    joined_block = ". ".join(block)
+    results.append(joined_block)
+print(f"Results: {results}")
 
-print(f"sentence: {sentence}")
-
-embeddings = model.encode(sentence)
+embeddings = model.encode(results)
 print(f"Embeddings Shape: {embeddings.shape}")
 
 print("-"*30)
